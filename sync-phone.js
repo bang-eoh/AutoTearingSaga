235839d0e02eb50a22b8b57ab8f7ff8c00bc6a78
Bang Dao <bang@eoh.io>
2022-10-23 11:14:47 +0700

    update save file

--- sync-phone.js
+++ sync-phone.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
-exec('adb -s R3CN203BDKN pull storage/self/primary/duckstation/savestates/SLPS-03177_0.sav', (err1, stdout, stderr) => {
-  exec('adb -s emulator-5554 push SLPS-03177_0.sav storage/self/primary/duckstation/savestates/SLPS-03177_0.sav', (err2, stdout, stderr) => {
+exec('adb -s R3CN203BDKN pull storage/self/primary/duckstation/savestates/SLPS-03177_0.sav /tmp/', (err1, stdout, stderr) => {
+  exec('adb -s emulator-5554 push /tmp/SLPS-03177_0.sav storage/self/primary/duckstation/savestates/SLPS-03177_0.sav', (err2, stdout, stderr) => {
     if (err1 || err2) {
       console.log(err1, err2)
     } else {
